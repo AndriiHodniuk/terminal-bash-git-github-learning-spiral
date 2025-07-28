c249141a3da4cc0b8ecdf29abffd066af8dda6d1
Andrii Hodniuk <andrii.hodniuk@outlook.com>
2025-07-28 14:28:19 +0200

    refactor: organize rename script with functions

--- scripts/rename.sh
+++ scripts/rename.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+#==============================================================================
+# FUNCTIONS
+#==============================================================================
+
+
+print_usage() {
+    echo "Error: Incorrect arguments provided. " >&2
     echo "Usage: $0 <directory> <prefix>" >&2
-    exit 1
-fi
+}
 
-TARGET_DIR=$1
-PREFIX=$2
+main() {
+    # --- 1. Checks ---
+    
 
-if [ ! -d "$TARGET_DIR" ]; then
-    echo "Error: Directory '$TARGET_DIR' not found." >&2
-    exit 1
-fi
-
-if [ -z "$(ls -A "$TARGET_DIR")" ]; then
-    echo "Info: Directory '$TARGET_DIR' is empty. Nothing to rename."
-    exit 0 
-fi
-
-for FILE_PATH in "$TARGET_DIR"/*; do
-    if [ -f "$FILE_PATH" ]; then
-        FILENAME=$(basename "$FILE_PATH")
-        mv "$FILE_PATH" "$TARGET_DIR/$PREFIX"_"$FILENAME"
-        echo "Renamed '$FILENAME' to '$PREFIX'_'$FILENAME'"  
+    if [ "$#" -ne 2 ]; then
+        print_usage
+        exit 1
     fi
-done
 
-echo "Bulk rename completed."
+    local TARGET_DIR=$1
+    local PREFIX=$2
+
+    if [ ! -d "$TARGET_DIR" ]; then
+        echo "Error: Directory '$TARGET_DIR' not found." >&2
+        exit 1
+    fi
+
+    if [ -z "$(ls -A "$TARGET_DIR")" ]; then
+        echo "Info: Directory '$TARGET_DIR' is empty. Nothing to rename."
+        exit 0 
+    fi
+
+
+    for FILE_PATH in "$TARGET_DIR"/*; do
+        if [ -f "$FILE_PATH" ]; then
+            local FILENAME=$(basename "$FILE_PATH")
+            mv "$FILE_PATH" "$TARGET_DIR/$PREFIX"_"$FILENAME"
+            echo "Renamed '$FILENAME' to '$PREFIX'_'$FILENAME'"  
+        fi
+    done
+
+    echo "Bulk rename completed."
+}
+
+#==============================================================================
+# SCRIPT EXECUTION
+#==============================================================================
+
+main "$@"
